57a498b6d95ccbb15a1c620cf0b317cd962058c1
benorita velazquez bienvenidos <benoritavelaz@icloud.com>
2026-05-05 17:23:50 +0300

    fix

--- alsat/client/src/utils/api.js
+++ alsat/client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://alsatyeri.onrender.com/',
+    baseURL: 'https://alsatyeri.onrender.com/api',
 });
 
 export default api;
